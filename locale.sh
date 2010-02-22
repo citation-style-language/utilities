0999233ea5cd9a50cd634290e15dac010e93ada7
rmzelle <none@none>
2010-02-22 22:03:25 +0100

    Update locale.sh to use locale files from CSL 0.8 branch

--- locale.sh
+++ locale.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
-# Locations:
-# http://bitbucket.org/bdarcus/csl-utils/src/tip/update-locales.xsl in script root
-# Babelzilla locale files in locales folder, e.g.: ./locales/xx-XX/zotero/locales.xml
-# delete sr-YU locale folder (not longer used)
-# saxon.jar should be present at jing-20081028/bin/saxon.jar
-# output goes to ./locales/locales-xx-XX.xml
+# Converts CSL 0.8 locale files to the CSL 1.0 format
 
-FILELIST="`find . -name locales.xml`"
+# Location of CSL 1.0 en-US locale file
+CSL10LocalesDirectory='../csl-locales/trunk/'
+
+# Location of CSL 0.8 locale files
+CSL08LocalesDirectory='../csl-locales/branches/0.8/'
+
+# Location of saxon.jar
+saxon='../jing-20081028/bin/saxon.jar'
+
+localeTemplate=${CSL10LocalesDirectory}locales-en-US.xml
+FILELIST="`find $CSL08LocalesDirectory -name locales-*.xml`"
+# echo $FILELIST
 for FILE in ${FILELIST}
-do
-languageCode=`echo ${FILE#./locales/}`
-languageCode=`echo ${languageCode%/zotero/locales.xml}`
-# echo $languageCode
-# echo locales-${languageCode}.xml
-# run on English converted locale file, replace location of input file in XSLT
-findstring='locales\\locales-nl-NL.xml'
-echo $findstring
-sed "s#$findstring#$FILE#g" update-locales.xsl > update-locales-temp.xsl
-java -jar jing-20081028/bin/saxon.jar -o locales/locales-${languageCode}.xml locales-en-US-1.0.xml update-locales-temp.xsl
-done
+  do
+    languageCode=`echo ${FILE#*locales-}`
+    languageCode=`echo ${languageCode%.xml}`
+    # echo locales-${languageCode}.xml
+    # Create a custom XLST stylesheet for each locale, run each sheet on the en-US template locale file
+    findstring='locales\\locales-nl-NL.xml'
+    sed "s#$findstring#$FILE#g" update-locales.xsl > update-locales-temp.xsl
+    java -jar $saxon -o ${CSL10LocalesDirectory}locales-${languageCode}.xml $localeTemplate update-locales-temp.xsl
+  done
 
+# Various tips and tricks:
 # http://www.cs.wright.edu/~pmateti/Courses/333/Notes/bash-vars.html
 # http://forums.oracle.com/forums/thread.jspa?threadID=982951&tstart=0
 # http://episteme.arstechnica.com/eve/forums/a/tpc/f/96509133/m/466009161041
 # http://www.linuxforums.org/forum/linux-programming-scripting/68862-sed-error-unknown-option-s.html
 # http://www.linuxquestions.org/questions/programming-9/sed-doesnt-accept-variable-in-bash-script-325935/
-
