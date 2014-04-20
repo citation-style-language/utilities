5a5c17d8a34386a288f991c928e0641c5304f606
rmzelle <rintze.zelle@gmail.com>
2014-04-20 16:38:33 -0400

    Update Tracking Dependents script

--- csl-tracking-dependents.py
+++ csl-tracking-dependents.py
@@ -4,7 +4,7 @@
 # Version: 2014-04-17
 # * Requires lxml library (http://lxml.de/)
 
-import os, glob, re, inspect
+import os, glob, re, inspect, shutil
 from lxml import etree
 
 # http://stackoverflow.com/questions/50499
@@ -13,10 +13,12 @@ folderPath =  os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfra
 parentFolderPath = os.path.dirname (folderPath)
 path = os.path.join(parentFolderPath, 'styles')
 pathGeneratedStyles = os.path.join(parentFolderPath, 'utilities', 'generate_dependent_styles', 'generated_styles', 'frontiers')
+pathRemovedStyles = os.path.join(parentFolderPath, 'removed-styles')
 
 dependentStyles = []
 commentMatchingStyles = []
 parentMatchingStyles = []
+timestampMatchingStyles = []
 generatedStyles = []
 
 for stylepath in glob.glob( os.path.join(path, 'dependent', 'frontiers*.csl') ):
@@ -25,27 +27,53 @@ for stylepath in glob.glob( os.path.join(path, 'dependent', 'frontiers*.csl') ):
 for stylepath in glob.glob( os.path.join(pathGeneratedStyles, '*.csl') ):
     generatedStyles.append(os.path.basename(stylepath))
 
+#Filter dependent styles by their parent (set A), print number
+#Of set A, print style ID if XML comment doesn't match that of dependent style template
+#Of set A, print style ID if timestamp doesn't match that of dependent style template
+#Have a toggle to move remaining styles out of root folder
+#(it would be better to filter by the XML comment on the first pass, since styles from
+#a set may have different parents, but XML comments aren't currently unique to a set)
 for style in dependentStyles:
     parser = etree.XMLParser(remove_blank_text=True)
     parsedStyle = etree.parse(style, parser)
     styleElement = parsedStyle.getroot()
-
-    comments = styleElement.xpath("//comment()", namespaces={"cs": "http://purl.org/net/xbiblio/csl"})
-    
-    for comment in comments:
-        if(comment.text == " Generated with https://github.com/citation-style-language/utilities/tree/master/generate_dependent_styles "):
-            commentMatchingStyles.append(os.path.basename(style))
-            #print os.path.basename(style)
     
     parentLink = styleElement.find(".//{http://purl.org/net/xbiblio/csl}link[@rel='independent-parent']")
     if(parentLink.attrib.get("href") == "http://www.zotero.org/styles/frontiers"):
         parentMatchingStyles.append(os.path.basename(style))
+        
+    comments = styleElement.xpath("//comment()", namespaces={"cs": "http://purl.org/net/xbiblio/csl"})
+    for comment in comments:
+        if(comment.text == " Generated with https://github.com/citation-style-language/utilities/tree/master/generate_dependent_styles "):
+            commentMatchingStyles.append(os.path.basename(style))
+    
+    timestamp = styleElement.find(".//{http://purl.org/net/xbiblio/csl}updated")
+    if(timestamp.text == "2012-09-15T12:00:00+00:00"):
+        timestampMatchingStyles.append(os.path.basename(style))
 
+print("Number of dependent styles with selected parent: " + str(len(parentMatchingStyles)))
+print("Number of generated styles: " + str(len(generatedStyles)))
 for style in parentMatchingStyles:
     if not (style in commentMatchingStyles):
-        print "bad comment!"
+        print "bad comment!: " + style
+        parentMatchingStyles.remove(style)
+    if not (style in timestampMatchingStyles):
+        print "bad timestamp!: " + style
+        parentMatchingStyles.remove(style)
     if not (style in generatedStyles):
-        print "not generated!" 
+        print "not generated!: " + style
+        parentMatchingStyles.remove(style)
+print("Number of consistent styles: " + str(len(parentMatchingStyles)))
+
+moveStyles = True
+
+if moveStyles == True:
+    #move styles out of "styles/dependent" folder
+    if not os.path.exists(pathRemovedStyles):
+        os.makedirs(pathRemovedStyles)
+    
+    for style in parentMatchingStyles:
+        shutil.move(os.path.join(path, 'dependent', style), os.path.join(pathRemovedStyles, style))
 
 #     counter = []
 #     for infoNodeIndex, infoNode in enumerate(csInfo):
